d52465c4d6e37d66f55a675d4e1770c1ba50708f
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-03-25 07:47:00 -0700

    Update compiler.js

--- compiler.js
+++ compiler.js
@@ -1,27 +1,50 @@
 const fs = require("fs");
 
-const operators = {
-    "+": {
-        id: "add",
-        priority: 0
-    },
-    "-": {
-        id: "sub",
-        priority: 0
-    },
-    "*": {
-        id: "mult",
-        priority: 1
-    },
-    "/": {
-        id: "div",
-        priority: 1
-    },
-    "^": {
-        id: "pow",
-        priority: 2
+class Operator {
+    constructor(...args) {
+        this.id = args[0];
+        if(typeof args[1] == "number") {
+            this.priority = args[1];
+            this.unary = args[2] ?? false;
+            this.binary = args[3] ?? true;
+        } else {
+            this.priortiy = 2;
+            this.unary = args[1] ?? false;
+            this.binary = args[2] ?? true;
+        }
     }
+}
+
+const operators = {
+    "+": new Operator("add", 0),
+    "-": new Operator("sub", 0, true),
+    "*": new Operator("mult", 1),
+    "/": new Operator("div", 1),
+    "**": new Operator("pow"),
+    "&": new Operator("b-and"),
+    "|": new Operator("b-or"),
+    "^": new Operator("b-xor"),
+    "&&": new Operator("and"),
+    "||": new Operator("or"),
+    "!": new Operator("not", true, false),
+    "!&": new Operator("nand"),
+    "!|": new Operator("nor")
 };
+
+const operator_chars = [];
+while(true) {
+    let arr = [];
+    let layer_success = false;
+    Object.keys(operators).forEach(op => {
+        let ch = op[operator_chars.length];
+        if(op.length > operator_chars.length && !arr.includes(ch)) {
+            layer_success = true;
+            arr.push(ch);
+        }
+    });
+    if(!layer_success) break;
+    operator_chars.push(arr);
+}
 
 var parse = function(code) {
     let parenthesis = [];
@@ -33,14 +56,27 @@ var parse = function(code) {
 
     for(let i = 0; i < code.length; i++) {
         let ch = code[i];
-        if(Object.keys(operators).includes(ch)) {
-            if(depth == 0 && (!curOperator || operators[ch].priority < operators[curOperator.type].priority)) {
+
+        // Find full operator
+        let full_read = "";
+        let lookahead = 0;
+        while(true) {
+            let lookch = code[i + lookahead];
+            if(operator_chars[lookahead] && operator_chars[lookahead].includes(code[i + lookahead])) {
+                full_read += lookch;
+            } else break;
+            lookahead++;
+        }
+        if(full_read.length != 0 && depth == 0) { 
+            // Is an operator with highest priority
+            if(!curOperator || operators[full_read].priority < operators[curOperator.type].priority) {
                 curOperator = {
-                    type: ch,
-                    idx: i
+                    type: full_read,
+                    idx: i,
+                    length: full_read.length
                 };
             }
-        } else if(ch == "(") {
+        } else if(ch == "(") { // Parenthesis
             if(depth == 0) paren = i;
             depth++;
         } else if(ch == ")") {
@@ -49,25 +85,30 @@ var parse = function(code) {
                 parenthesis.push(paren, i);
                 paren = null;
             }
-        } else {
-
-        }
+        } // No else yet
     }
 
     if(curOperator) {
         let left = parse(code.substring(0, curOperator.idx));
-        let right = parse(code.substring(curOperator.idx + 1));
-        if(left.value.length == 0) {
+        let right = parse(code.substring(curOperator.idx + curOperator.length));
+        let op = operators[curOperator.type];
+        if(String(right.value).length == 0) {
+            throw "Operator with no operand";
+        } else if(String(left.value).length == 0) {
+            if(op.binary) throw "Binary operator used with one operand";
+
             return {
-                type: "operator",
-                operation: operators[curOperator.type].id,
+                parsed: "operator",
+                operation: op.id,
                 unary: true,
                 value: right
-            }
+            };
         } else {
+            if(op.unary) throw "Unary operator used with two operands";
+
             return {
-                type: "operator",
-                operation: operators[curOperator.type].id,
+                parsed: "operator",
+                operation: op.id,
                 unary: false,
                 left: left,
                 right: right
@@ -76,12 +117,29 @@ var parse = function(code) {
     } else if(parenthesis.length) {
         return parse(code.substring(parenthesis[0] + 1, parenthesis[1]));
     } else {
-        return {
-            type: "literal",
-            value: code.trim()
+        let val = code.trim();
+        let num = Number(val);
+        let bool = Boolean(val);
+        if(!isNaN(num) && val.length != 0) {
+            return {
+                parsed: "literal",
+                type: "number",
+                value: num
+            };
+        } else if(bool) {
+            return {
+                parsed: "literal",
+                type: "boolean",
+                value: bool
+            };
+        } else {
+            return {
+                parsed: "literal",
+                type: "string",
+                value: val
+            };
         }
     }
-
 };
 
 fs.readFile("test.txt", "utf8", (error, data) => {
